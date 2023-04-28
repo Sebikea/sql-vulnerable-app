939dbe0e2c79bcb9ebba896fddaf0b388cdab17c
Seb B <sebi@Sebs-MacBook-Pro.local>
2023-04-28 11:46:30 +0200

    Fixed everything

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def get_db_connection():
+    conn = sqlite3.connect('products.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route("/search")
-def search():
-    query = request.args.get("query")
-    conn = sqlite3.connect("database.db")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    conn = get_db_connection()
     cursor = conn.cursor()
+    query = "SELECT * FROM products"
+    if request.method == 'POST':
+        search_term = request.form['search_term']
+        query = "SELECT * FROM products WHERE name LIKE ?"
+        cursor.execute(query, ('%'+search_term+'%',))
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', rows=rows)
 
-    cursor.execute("SELECT * FROM products WHERE name = '{}'".format(query))
-    results = cursor.fetchall()
-    return render_template("results.html", results=results)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
